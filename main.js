9f36c4b056d2d054c88ba2c45ad715097966a5c0
ketig <41895005+ketig@users.noreply.github.com>
2024-02-25 15:03:14 +0400

    substring

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function addToDisplay(value) {
-    
+    document.getElementById("display").value += value;
+}
+function clearDisplay() {
+    clearInput("display");
+}
+function clearAll() {
+    clearDisplay();
+    clearInput("displayResult");
+}
+function clearInput(id) {
+    document.getElementById(id).value = "";
+}
+function leftDelete() {
+    let displayValue = document.getElementById("display").value;
+    let result = displayValue.substring(0, displayValue.length-1);
+    document.getElementById("display").value = result;
 }
